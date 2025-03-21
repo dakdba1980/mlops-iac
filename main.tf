a712cc4f3d4760df2f98d34269d501b688ddc8f4
dakdba1980 <dak.dba@gmail.com>
2025-03-21 16:13:24 +0530

    snyk workflow fixes

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "example-instance"
+    }
+}
+
+# module "eks" {
+#     source          = "terraform-aws-modules/eks/aws"
+#     cluster_name    = "example-cluster"
+#     cluster_version = "1.21"
+#     subnets         = ["subnet-0123456789abcdef0", "subnet-0123456789abcdef1"]
+#     vpc_id          = "vpc-0123456789abcdef0"
+
+#     node_groups = {
+#         example = {
+#             desired_capacity = 1
+#             max_capacity     = 2
+#             min_capacity     = 1
+
+#             instance_type = "t2.micro"
+#         }
+#     }
+# }
